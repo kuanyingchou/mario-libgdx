3748a0d3dc81074f816cd464e6b2ba14b414c718
ken <kuanyingchou@gmail.com>
2014-02-10 21:24:22 +0800

    rename contact listener

--- MarioContactListener.java
+++ MarioContactListener.java
@@ -8,7 +8,13 @@ class MarioContactListener implements ContactListener {
     public void beginContact(Contact contact) {
         final Object[] objs = getUserData(contact);
         if(objs[0] == null || objs[1] == null) return;
-        System.out.println(objs[0] + " hits "+ objs[1]);
+        //System.out.println(objs[0] + " hits "+ objs[1]);
+        for(int i=0; i<2; i++) {
+            if(objs[i] instanceof Mario) {
+                ((Mario)objs[i]).hit();
+                break;
+            }
+        }
     }
     public void endContact(Contact contact) {
         //System.out.println("end contact");
@@ -22,6 +28,7 @@ class MarioContactListener implements ContactListener {
     private Object[] getUserData(Contact contact) {
         final Fixture fa = contact.getFixtureA();
         final Fixture fb = contact.getFixtureB();
+        if(fa.isSensor() || fb.isSensor()) return new Object[] {null, null};
         final Object a = fa.getBody().getUserData();
         final Object b = fb.getBody().getUserData();
         return new Object[] {a, b};
